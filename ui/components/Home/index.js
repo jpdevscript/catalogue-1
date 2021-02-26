70edaeb1515fcdf626c24f55bef45421e5e6fbb7
Jignesh Patel <jigneshpatel@Jigneshs-MacBook-Pro.local>
2021-02-26 17:59:47 -0500

    feat(test-1): home

---
+++ ui/components/Home/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import Heading from "@tds/core-heading";
+
+const Home = () => <Heading level="h2">Home</Heading>;
+
+Home.defaultProps = {};
+Home.propTypes = {};
+
+Home.displayName = "Home";
+
+export default Home;
